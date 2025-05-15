9eca2d0c1c21ec028acb53e17f123e283c012747
Juncst <castajose7@gmail.com>
2025-05-15 15:39:55 -0600

    nodo.java arreglado

--- nodo.java
+++ nodo.java
@@ -1,13 +1,10 @@
 public class Nodo {
     int valor;
-    Nodo izquierda;
-    Nodo derecha;
+    Nodo izquierda, derecha;
     int altura;
 
     public Nodo(int valor) {
         this.valor = valor;
-        this.izquierda = null;
-        this.derecha = null;
-        this.altura = 1; // La altura de un nuevo nodo (hoja) es 1
+        this.altura = 1;
     }
 }
